304e2b48625bae0d836d6e9b4eeb44d1b9203ca1
pedro.molina@petroprix.com-at-658554454469 <pedro.molina@petroprix.com>
2021-02-17 19:36:43 +0100

    terminada animacion

--- lib/src/pages/circular_progress_page.dart
+++ lib/src/pages/circular_progress_page.dart
@@ -1,4 +1,5 @@
 import 'dart:math';
+import 'dart:ui';
 
 import 'package:flutter/material.dart';
 
@@ -7,16 +8,54 @@ class CircularProgressPage extends StatefulWidget {
   _CircularProgressPageState createState() => _CircularProgressPageState();
 }
 
-class _CircularProgressPageState extends State<CircularProgressPage> {
+class _CircularProgressPageState extends State<CircularProgressPage>
+    with SingleTickerProviderStateMixin {
+  double porcentaje = 0.0;
+  double nuevoPorcentaje = 0.0;
+  AnimationController controller;
+  @override
+  void initState() {
+    super.initState();
+    controller = new AnimationController(
+        vsync: this, duration: Duration(milliseconds: 800));
+    controller.addListener(() {
+      //print(controller.value.toString());
+      setState(() {
+        porcentaje = lerpDouble(porcentaje, nuevoPorcentaje, controller.value);
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.refresh),
+        backgroundColor: Colors.pink,
+        onPressed: () {
+          porcentaje = nuevoPorcentaje;
+          nuevoPorcentaje += 10;
+
+          if (nuevoPorcentaje > 100) {
+            porcentaje = 0;
+            nuevoPorcentaje = 0;
+          }
+          controller.forward(from: 0.0);
+          setState(() {});
+        },
+      ),
       body: Center(
         child: Container(
           padding: EdgeInsets.all(5),
           width: 300,
           height: 300,
-          child: CustomPaint(painter: _MiRadialProgress(20)),
+          child: CustomPaint(painter: _MiRadialProgress(porcentaje)),
         ),
       ),
     );
