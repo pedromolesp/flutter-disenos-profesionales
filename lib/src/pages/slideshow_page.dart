f55c7bb37cfeeccfe9291ae6962f013cc47994f0
pedromolesp <pedromolesp@gmail.com>
2021-03-04 20:30:18 +0100

    65 terminado

--- lib/src/pages/slideshow_page.dart
+++ lib/src/pages/slideshow_page.dart
@@ -5,7 +5,95 @@ class SlideShowPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SvgPicture.asset("assets/svgs/slide-1.svg"),
+      body: Center(
+          child: Column(
+        children: [
+          Expanded(child: _Slides()),
+          _Dots(),
+        ],
+      )),
     );
+  }
+}
+
+class _Dots extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 70,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          _Dot(0),
+          _Dot(1),
+          _Dot(2),
+        ],
+      ),
+    );
+  }
+}
+
+class _Dot extends StatelessWidget {
+  final int index;
+  _Dot(this.index);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 5),
+      width: 12,
+      height: 12,
+      decoration: BoxDecoration(color: Colors.grey, shape: BoxShape.circle),
+    );
+  }
+}
+
+class _Slides extends StatefulWidget {
+  @override
+  __SlidesState createState() => __SlidesState();
+}
+
+class __SlidesState extends State<_Slides> {
+  PageController _controller = new PageController();
+
+  @override
+  void initState() {
+    super.initState();
+    _controller.addListener(() {
+      print(_controller.page);
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: PageView(
+        controller: _controller,
+        children: [
+          _Slide("assets/svgs/slide-1.svg"),
+          _Slide("assets/svgs/slide-2.svg"),
+          _Slide("assets/svgs/slide-3.svg"),
+        ],
+      ),
+    );
+  }
+}
+
+class _Slide extends StatelessWidget {
+  final String svg;
+  _Slide(this.svg);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        width: double.infinity,
+        height: double.infinity,
+        padding: EdgeInsets.all(30),
+        child: SvgPicture.asset(svg));
   }
 }
