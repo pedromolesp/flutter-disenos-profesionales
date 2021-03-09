a7bc920f96fb7cc04ac2159c7aa83d7ab6ff715e
pedromolesp <pedromolesp@gmail.com>
2021-03-09 22:45:57 +0100

    desglosar dots

--- lib/src/pages/slideshow_page.dart
+++ lib/src/pages/slideshow_page.dart
@@ -1,11 +1,20 @@
 import 'package:disenos/src/widgets/slideshow.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class SlideShowPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SlideShow(),
+      body: SlideShow(
+        slides: [
+          SvgPicture.asset("assets/svgs/slide-1.svg"),
+          SvgPicture.asset("assets/svgs/slide-2.svg"),
+          SvgPicture.asset("assets/svgs/slide-3.svg"),
+          SvgPicture.asset("assets/svgs/slide-4.svg"),
+          SvgPicture.asset("assets/svgs/slide-5.svg")
+        ],
+      ),
     );
   }
 }
