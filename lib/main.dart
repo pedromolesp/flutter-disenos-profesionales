81381372c405c38413cfddb773a3cfb7fcb1630d
Pedro Molina <pedro.molina@petroprix.com>
2021-04-29 21:13:23 +0200

    darkmode

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:disenos/src/pages/launcher_page.dart';
+import 'package:disenos/src/theme/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 // import 'package:disenos/src/pages/headers_page.dart';
 // import 'package:disenos/src/pages/animaciones_page.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(
+    ChangeNotifierProvider(create: (_) => new ThemeChanger(), child: MyApp()));
 
 class MyApp extends StatelessWidget {
   @override
@@ -12,6 +15,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Diseños App',
+        theme: ThemeData.dark(),
         home: LauncherPage());
   }
 }
